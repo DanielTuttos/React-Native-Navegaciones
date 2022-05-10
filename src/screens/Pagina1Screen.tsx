7e6e350d86cdb0ac4aca42a901817f30a0a78029
Daniel Romero AG <tutorialesdanielromero@gmail.com>
2022-05-09 19:07:19 -0500

    configuracion y funcionamiento basico de stacknavigation

--- src/screens/Pagina1Screen.tsx
+++ src/screens/Pagina1Screen.tsx
@@ -1,21 +1,52 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, TouchableOpacity, View } from 'react-native';
 import { styles } from '../theme/appTheme';
 
 interface Props extends StackScreenProps<any, any> { }; // lo extiende de la propiedades del stack para el tipado
 
 export const Pagina1Screen = ({ navigation }: Props) => {
 
-    
+
 
     return (
         <View style={styles.globalMargin}>
-            <Text>Pagina1Screen</Text>
+            <Text style={styles.title}>Pagina1Screen</Text>
             <Button
                 title='Ir pagina 2'
                 onPress={() => navigation.navigate('Pagina2Screen')}
             />
-        </View>
+
+            <Text>Navegar con argumentos</Text>
+
+            <View style={{ flexDirection: 'row' }}>
+                <TouchableOpacity
+                style={{
+                    ...styles.botonGrande,
+                    backgroundColor:'#5856d6'
+                }}
+                    onPress={() => navigation.navigate('PersonaScreen', {
+                        id: 1,
+                        nombre: "Pedro"
+                    })}
+                >
+                    <Text style={styles.botonGrandeTexto}>Pedro</Text>
+                </TouchableOpacity>
+
+
+                <TouchableOpacity
+                style={{
+                    ...styles.botonGrande,
+                    backgroundColor:'#ff9427'
+                }}
+                    onPress={() => navigation.navigate('PersonaScreen', {
+                        id: 2,
+                        nombre: "Maria"
+                    })}
+                >
+                    <Text style={styles.botonGrandeTexto}>Maria</Text>
+                </TouchableOpacity>
+            </View>
+        </View >
     )
 }
--- src/screens/Pagina2Screen.tsx
+++ src/screens/Pagina2Screen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button, Text, View } from 'react-native';
 import { styles } from '../theme/appTheme';
 import { useNavigation } from '@react-navigation/core';
@@ -8,11 +8,18 @@ import { useNavigation } from '@react-navigation/core';
 
 export const Pagina2Screen = () => {
 
-    const navigator = useNavigation(); //hacerlo con hooks || mejor es usarlo desde las props 
+    const navigator = useNavigation(); //hacerlo con hooks || mejor es usarlo desde las props
+    
+    useEffect(() => {
+        navigator.setOptions({
+            title: 'Hola Mundo', //cambia el titulo por defecto de la pagina
+            headerBackTitle: 'Volver', 
+        })
+    }, [])
 
     return (
         <View style={styles.globalMargin}>
-            <Text>Pagina2Screen</Text>
+            <Text style={styles.title}>Pagina2Screen</Text>
             <Button
                 title='Ir pagina 3'
                 onPress={() => navigator.navigate('Pagina3Screen')}
